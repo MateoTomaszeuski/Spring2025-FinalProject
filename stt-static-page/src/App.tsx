0982d176a819bf89aa03200bff0fd9cb5fc266d5
Cody-Howell <codyhowell44@gmail.com>
2025-03-18 17:57:46 -0600

    Added all of the static page info

---
+++ stt-static-page/src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './App.css'
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Consilium</h1>
+        <p>Student time management app with a social twist!</p>
+        <a href='https://youtu.be/dQw4w9WgXcQ?si=cxQcwxCL5z5MN--Q' target='_blank'>Download Now!</a>
+      </div>
+    )
+  }
+}
+
+export default App
