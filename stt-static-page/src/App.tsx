ec77ff808bed5e93e454546148aa39f9ffdfe59e
Cody-Howell <codyhowell44@gmail.com>
2025-03-19 17:30:17 -0600

    Static page works

--- stt-static-page/src/App.tsx
+++ stt-static-page/src/App.tsx
@@ -4,10 +4,11 @@ import './App.css'
 class App extends React.Component {
   render() {
     return (
-      <div>
+      <div id='app'>
         <h1>Consilium</h1>
         <p>Student time management app with a social twist!</p>
         <a href='https://youtu.be/dQw4w9WgXcQ?si=cxQcwxCL5z5MN--Q' target='_blank'>Download Now!</a>
+        
       </div>
     )
   }
