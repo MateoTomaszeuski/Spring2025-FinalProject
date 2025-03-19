173e98e900671077cde04e7147e61a82d4f90c0c
mateo.tomaszeuski <mtomaszueki@gmail.com>
2025-03-19 16:03:04 -0600

    class changes

--- Consilium/Consilium.Maui/ViewModels/ToolsViewModel.cs
+++ Consilium/Consilium.Maui/ViewModels/ToolsViewModel.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using CommunityToolkit.Mvvm;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,6 +9,29 @@ using System.Threading.Tasks;
 
 namespace Consilium.Maui.ViewModels;
 
-public partial class ToolsViewModel
+public partial class ToolsViewModel : ObservableObject
 {
+    [ObservableProperty]
+    private string activeTool = "Notes";
+
+    [RelayCommand]
+    public void ChangeToolCommand(string tool)
+    {
+        switch (tool)
+        {
+            case "Notes":
+                ActiveTool = "Notes";
+                break;
+            case "Calculator":
+                ActiveTool = "Calculator";
+                break;
+            case "Pomodoro":
+                ActiveTool = "Pomodoro";
+                break;
+            default:
+                ActiveTool = "Notes";
+                break;
+        }
+        ;
+    }
 }
