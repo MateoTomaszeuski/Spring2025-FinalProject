f196dcd2e14fb78bc4f5533309daef5e613396be
mateo <mtomaszeuski@gmail.com>
2025-03-21 13:34:21 -0600

    mateo

--- Consilium/Consilium.Maui/ViewModels/ToolsViewModel.cs
+++ Consilium/Consilium.Maui/ViewModels/ToolsViewModel.cs
@@ -1,12 +1,32 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
+﻿using System.ComponentModel;
+using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 
 namespace Consilium.Maui.ViewModels;
 
 public partial class ToolsViewModel : ObservableObject
 {
+    public ToolsViewModel()
+    {
+        PropertyChanged += OnActiveToolChanged
+    }
     [ObservableProperty]
     private string activeTool = "Notes";
+
+    [ObservableProperty]
+    private bool notesActive = false;
+    [ObservableProperty]
+    private bool calculatorActive = false;
+    [ObservableProperty]
+    private bool pomodoroActive = false;
+
+    private void OnActiveToolChanged(object sender, PropertyChangedEventArgs e)
+    {
+        if (e.PropertyName == nameof(ActiveTool))
+        {
+            Console.WriteLine($"ActiveTool changed to: {ActiveTool}");
+        }
+    }
 
     [RelayCommand]
     public void ChangeTool(string tool)
@@ -25,6 +45,7 @@ public partial class ToolsViewModel : ObservableObject
             default:
                 ActiveTool = "Notes";
                 break;
-        };
+        }
+        ;
     }
 }
