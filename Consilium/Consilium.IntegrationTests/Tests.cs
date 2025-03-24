e72904b48be39924e32708b2c7d4b83fb725ab87
mateo <mtomaszeuski@gmail.com>
2025-03-24 16:12:57 -0600

    pray with me

--- Consilium/Consilium.IntegrationTests/Tests.cs
+++ Consilium/Consilium.IntegrationTests/Tests.cs
@@ -1,10 +1,13 @@
-﻿namespace Consilium.IntegrationTests;
+﻿
+namespace Consilium.IntegrationTests;
 
 public class Tests
 {
     [Test]
     public void Basic()
     {
+        var client = new HttpClient();
+        client.BaseAddress = new Uri("https://localhost:8080");
         Console.WriteLine("This is a basic test");
     }
     
