b13b03ce4af481dad1cdf2197790bdb78f10b8c9
Tallen258 <112665786+Tallen258@users.noreply.github.com>
2025-03-24 17:27:15 -0600

    c

--- Consilium/Consilium.IntegrationTests/Tests.cs
+++ Consilium/Consilium.IntegrationTests/Tests.cs
@@ -9,10 +9,17 @@ public class Tests
     public async Task HelloWorldCall()
     {
         var client = new HttpClient();
+<<<<<<< HEAD
         client.BaseAddress = new Uri("http://localhost:8080");
 
         string response = await client.GetStringAsync("/");
 
+=======
+        client.BaseAddress = new Uri("http://localhost:8080");
+
+        string response = await client.GetStringAsync("/");
+
+>>>>>>> taft
         await Assert.That(response).IsEqualTo("Welcome to the Consilium Api");
     }
 }
