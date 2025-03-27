5ab071104da5e87ff73b3bfd2e17f04833fa0584
Cody-Howell <codyhowell44@gmail.com>
2025-03-27 17:38:13 -0600

    Updated service

--- Consilium/Consilium.API/DBServices/DBService.cs
+++ Consilium/Consilium.API/DBServices/DBService.cs
@@ -5,8 +5,8 @@ using System.Data;
 namespace Consilium.API.DBServices;
 
 public class DBService(IDbConnection conn) {
-    public IEnumerable<string> GetAllUsers() {
-        string sql = "select * from account";
-        return conn.Query<string>(sql);
+    public IEnumerable<User> GetAllUsers() {
+        string sql = "select id, email, displayname from account";
+        return conn.Query<User>(sql);
     }
 }
---
+++ Consilium/Consilium.Shared/Models/User.cs
@@ -0,0 +1,7 @@
+﻿namespace Consilium.Shared.Models;
+
+public class User {
+    public int id { get; set; }
+    public string email { get; set; }
+    public string displayName { get; set; }
+}
