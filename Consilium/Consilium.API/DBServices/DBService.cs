d3d5ac3336e73a97c0a46677c8afdd6de624fd30
mateo <mtomaszeuski@gmail.com>
2025-03-31 14:23:34 -0600

    format

--- Consilium/Consilium.API/DBServices/DBService.cs
+++ Consilium/Consilium.API/DBServices/DBService.cs
@@ -20,19 +20,19 @@ public class DBService(IDbConnection conn) : IDBService {
 
     public IEnumerable<TodoList> GetToDoLists(string username) {
         string sql = $"SELECT id, listname FROM todolist tl INNER JOIN account a on (tl.account_id = a.id) Where a.displayname = @username";
-        var todolists = conn.Query<TodoItem>(sql,new{username});
+        var todolists = conn.Query<TodoItem>(sql, new { username });
         return conn.Query<TodoList>(sql);
     }
     public TodoList GetTodoList(int tableid) {
         string sql = $"SELECT id, listname FROM todolist WHERE id = @tableid";
-        var todolist = conn.Query<TodoList>(sql,new {tableid}).FirstOrDefault();
-        
-         sql = $"SELECT id, categoryId, parentId,assignmentId,toDoName FROM todoitem WHERE toDoListId = @tableid";
-        var todoitems = conn.Query<TodoItem>(sql, new{tableid});
+        var todolist = conn.Query<TodoList>(sql, new { tableid }).FirstOrDefault();
 
-        if(todolist is null)
-        throw new SqlNullValueException();
-        todolist.TodoItems =  new(todoitems);
+        sql = $"SELECT id, categoryId, parentId,assignmentId,toDoName FROM todoitem WHERE toDoListId = @tableid";
+        var todoitems = conn.Query<TodoItem>(sql, new { tableid });
+
+        if (todolist is null)
+            throw new SqlNullValueException();
+        todolist.TodoItems = new(todoitems);
         return todolist;
 
 
