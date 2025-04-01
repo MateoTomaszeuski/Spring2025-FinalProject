2b2f56b416eb780f3c969d6b4ed51c0a5bb04ecf
Audrey Inglish <audreyinglish@gmail.com>
2025-04-01 00:07:31 -0600

    idk what you're talking about, categories always sorted without issue

--- Consilium/Consilium.Maui/Views/TodoListView.xaml.cs
+++ Consilium/Consilium.Maui/Views/TodoListView.xaml.cs
@@ -1,14 +1,20 @@
 using Consilium.Shared.ViewModels;
 namespace Consilium.Maui.Views;
 public partial class TodoListView : ContentPage {
+    private TodoListViewModel vm;
     public TodoListView(TodoListViewModel vm) {
         InitializeComponent();
+        this.vm = vm;
         BindingContext = vm;
         Appearing += async (s, e) =>
         {
             await vm.InitializeItemsAsync();
         };
 
-        //vm.SpinIcon = SpinIcon;
+    }
+    private void NewTodoTitle_Completed(object sender, EventArgs e) {
+        if (vm.AddTodoCommand.CanExecute(null) == true) {
+            vm.AddTodoCommand.Execute(null);
+        }
     }
 }
