335ba0e76ec8d8b569a4f0709a4b159756159a7d
mateo <mtomaszeuski@gmail.com>
2025-04-01 15:41:11 -0600

    handle

--- Consilium/Consilium.API/Program.cs
+++ Consilium/Consilium.API/Program.cs
@@ -15,39 +15,40 @@ var builder = WebApplication.CreateBuilder(args);
 DateTime started = DateTime.UtcNow;
 
 const string serviceName = "Consilium";
-var Uri = builder.Configuration["OTEL_URL"] ?? throw new Exception("No collector uri string was found.");
-builder.Services.AddOpenTelemetry()
-    .ConfigureResource(r => r.AddService(serviceName: serviceName))
-    .WithLogging(logging => logging
-        .AddOtlpExporter(options =>
-        {
-            options.Endpoint = new Uri(Uri);
-            options.Protocol = OtlpExportProtocol.Grpc;
-        }))
-    .WithMetrics(metrics => metrics
-        .AddMeter(serviceName)
-        .AddAspNetCoreInstrumentation()
-        .AddConsoleExporter()
-        .AddOtlpExporter(options =>
-        {
-            options.Endpoint = new Uri(Uri);
-            options.Protocol = OtlpExportProtocol.Grpc;
-        })
-        .AddView("http_response_duration_seconds", new ExplicitBucketHistogramConfiguration {
-            Boundaries = new double[] { 0.1, 0.5, 1, 2, 5, 10 }
-        }))
-    .WithTracing(tracing => tracing
-        .AddSource(serviceName)
-        .AddAspNetCoreInstrumentation()
-        .AddConsoleExporter()
-        .AddOtlpExporter(options =>
-        {
-            options.Endpoint = new Uri(Uri);
-            options.Protocol = OtlpExportProtocol.Grpc;
-        })
+var Uri = builder.Configuration["OTEL_URL"] ?? "";
+if (Uri != "") {
+    builder.Services.AddOpenTelemetry()
+        .ConfigureResource(r => r.AddService(serviceName: serviceName))
+        .WithLogging(logging => logging
+            .AddOtlpExporter(options =>
+            {
+                options.Endpoint = new Uri(Uri);
+                options.Protocol = OtlpExportProtocol.Grpc;
+            }))
+        .WithMetrics(metrics => metrics
+            .AddMeter(serviceName)
+            .AddAspNetCoreInstrumentation()
+            .AddConsoleExporter()
+            .AddOtlpExporter(options =>
+            {
+                options.Endpoint = new Uri(Uri);
+                options.Protocol = OtlpExportProtocol.Grpc;
+            })
+            .AddView("http_response_duration_seconds", new ExplicitBucketHistogramConfiguration {
+                Boundaries = new double[] { 0.1, 0.5, 1, 2, 5, 10 }
+            }))
+        .WithTracing(tracing => tracing
+            .AddSource(serviceName)
+            .AddAspNetCoreInstrumentation()
+            .AddConsoleExporter()
+            .AddOtlpExporter(options =>
+            {
+                options.Endpoint = new Uri(Uri);
+                options.Protocol = OtlpExportProtocol.Grpc;
+            })
 
-    );
-
+        );
+}
 string connString = builder.Configuration["DB_CONN"] ?? throw new Exception("No connection string was found.");
 Console.WriteLine("Connection String: " + connString);
 builder.Services.AddSingleton<IDbConnection>(provider =>
