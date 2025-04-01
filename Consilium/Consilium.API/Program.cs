9c099c6eced4ddbce430b762f61940a2b4321c1b
mateo <mtomaszeuski@gmail.com>
2025-04-01 14:18:51 -0600

    collector configs, DockerComposes put in folder

--- Consilium/Consilium.API/Program.cs
+++ Consilium/Consilium.API/Program.cs
@@ -3,9 +3,48 @@ using Consilium.API.DBServices;
 using EmailAuthenticator;
 using Microsoft.AspNetCore.Mvc;
 using Npgsql;
+using OpenTelemetry.Exporter;
+using OpenTelemetry.Logs;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
 using System.Data;
 
 var builder = WebApplication.CreateBuilder(args);
+
+const string serviceName = "Consilium";
+var Uri = builder.Configuration["OTEL_URL"] ?? throw new Exception("No collector uri string was found.");
+builder.Services.AddOpenTelemetry()
+    .ConfigureResource(r => r.AddService(serviceName: serviceName))
+    .WithLogging(logging => logging
+        .AddOtlpExporter(options =>
+        {
+            options.Endpoint = new Uri(Uri);
+            options.Protocol = OtlpExportProtocol.Grpc;
+        }))
+    .WithMetrics(metrics => metrics
+        .AddMeter(serviceName)
+        .AddAspNetCoreInstrumentation()
+        .AddConsoleExporter()
+        .AddOtlpExporter(options =>
+        {
+            options.Endpoint = new Uri(Uri);
+            options.Protocol = OtlpExportProtocol.Grpc;
+        })
+        .AddView("http_response_duration_seconds", new ExplicitBucketHistogramConfiguration {
+            Boundaries = new double[] { 0.1, 0.5, 1, 2, 5, 10 }
+        }))
+    .WithTracing(tracing => tracing
+        .AddSource(serviceName)
+        .AddAspNetCoreInstrumentation()
+        .AddConsoleExporter()
+        .AddOtlpExporter(options =>
+        {
+            options.Endpoint = new Uri(Uri);
+            options.Protocol = OtlpExportProtocol.Grpc;
+        })
+
+    );
 
 string connString = builder.Configuration["DB_CONN"] ?? throw new Exception("No connection string was found.");
 Console.WriteLine("Connection String: " + connString);
@@ -13,6 +52,7 @@ builder.Services.AddSingleton<IDbConnection>(provider =>
 {
     return new NpgsqlConnection(connString);
 });
+
 // Add services to the container.
 builder.Services.AddSingleton<AuthService>();
 builder.Services.AddSingleton<IEmailService, EmailService>();
