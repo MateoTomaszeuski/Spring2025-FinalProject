79203a3a2f4293a10c446a3b2e6edf692c7bbeed
Cody-Howell <codyhowell44@gmail.com>
2025-04-02 17:10:13 -0600

    Updated services, now to refactor everything

--- Consilium/Consilium.Shared/Services/ToDoService.cs
+++ Consilium/Consilium.Shared/Services/ToDoService.cs
@@ -2,17 +2,23 @@
 
 using Consilium.Shared.Models;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Net.Http.Json;
 
 namespace Consilium.Shared.Services;
 
 public class ToDoService : IToDoService {
     private readonly HttpClient client;
+    private readonly IPersistenceService service;
     private List<TodoItem> todoItems;
 
-    public ToDoService(IHttpClientFactory factory) {
+    private Dictionary<string, TodoList> lists;
+    public TodoList this[string s] => lists[s];
+
+    public ToDoService(IHttpClientFactory factory, IPersistenceService service) {
         client = factory.CreateClient("ApiClient");
         client.DefaultRequestHeaders.Add("Email-Auth_Email", "bob@example.com");
+        this.service = service;
         todoItems = new();
     }
 
