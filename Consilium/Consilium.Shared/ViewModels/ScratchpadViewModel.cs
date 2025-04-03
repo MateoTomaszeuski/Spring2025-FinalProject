7d1cc728ad4eefe4bb9ce5ee6c6bfa026bc26cad
Tallen258 <112665786+Tallen258@users.noreply.github.com>
2025-04-03 13:27:58 -0600

    dotnet format

--- Consilium/Consilium.Shared/ViewModels/ScratchpadViewModel.cs
+++ Consilium/Consilium.Shared/ViewModels/ScratchpadViewModel.cs
@@ -4,9 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Consilium.Shared.ViewModels
-{
-    class ScratchpadViewModel
-    {
+namespace Consilium.Shared.ViewModels {
+    class ScratchpadViewModel {
     }
 }
