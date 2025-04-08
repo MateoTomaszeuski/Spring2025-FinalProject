a4b0a9e01bbbd50ae7c3cf0176cd7693ae3f4ee8
mateo <mtomaszeuski@gmail.com>
2025-04-08 15:58:56 -0600

    pls send me the message

--- Consilium/Consilium.API/Program.cs
+++ Consilium/Consilium.API/Program.cs
@@ -77,7 +77,8 @@ builder.Services.AddSingleton<IDBService, DBService>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment()) {
+if (app.Environment.IsDevelopment()) 
+{
     app.UseSwagger();
     app.UseSwaggerUI();
 }
