f6e5f99ee2be6939e1212b9afb908134b61664b0
Audrey Inglish <audreyinglish@gmail.com>
2025-04-11 00:29:21 -0600

    kind of mobile responsive

--- stt-static-page/src/App.tsx
+++ stt-static-page/src/App.tsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, { useState } from 'react'
+
 import './App.css'
 import { BrowserRouter, Link, Route, Routes } from 'react-router-dom'
 import { MissingPage } from './components/MissingPage'
@@ -28,24 +29,23 @@ class App extends React.Component {
   }
 }
 
-class Header extends React.Component {
-  render() {
-    return (
-      <header>
-        <Link to="/" className='headerNav'><h1>Consilium</h1></Link>
-        <div className='navElements'>
-          <ul>
-            <li> <Link to="/" className='headerNav'>Home</Link></li>
-            <li><Link to="/demo" className='headerNav'>Demo</Link></li>
-            <li><Link to="/docs" className='headerNav'>Docs</Link></li>
-        <li><a id="download-button" href='https://youtu.be/dQw4w9WgXcQ?si=cxQcwxCL5z5MN--Q' target='_blank'>Download Now!</a></li>
-          </ul>
-        </div>
+function Header() {
+  return (
+    <header>
+      <Link to="/" className='headerNav'>
+        <h1>Consilium</h1>
+      </Link>
 
-
-      </header>
-    )
-  }
+      <nav className='navElements'>
+        <ul>
+          <li><Link to="/" className='headerNav'>Home</Link></li>
+          <li><Link to="/demo" className='headerNav'>Demo</Link></li>
+          <li><Link to="/docs" className='headerNav'>Docs</Link></li>
+          <li><a id="download-button" href='https://youtu.be/dQw4w9WgXcQ?si=cxQcwxCL5z5MN--Q' target='_blank'>Download Now!</a></li>
+        </ul>
+      </nav>
+    </header>
+  )
 }
 
 export default App
