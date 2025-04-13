aecf030e8713ef3b54fa676fefa9fcb1d7e918c8
Audrey Inglish <audreyinglish@gmail.com>
2025-04-12 20:19:15 -0600

    minor adjustments to text

--- stt-static-page/src/components/SignedInPage.tsx
+++ stt-static-page/src/components/SignedInPage.tsx
@@ -4,7 +4,7 @@ export class SignedInPage extends React.Component {
   render() {
     return (
       <div>
-        <h1>Signed In</h1>
+        <h2>Signed In</h2>
         <p>Thanks for signing in! You have been authenticated and you can now go back to the Mobile App.</p>
       </div>
     );
