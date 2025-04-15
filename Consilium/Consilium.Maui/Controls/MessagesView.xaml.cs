96deec9da7394ceee9c5dee309b8889c9cef1f0a
mateo <mtomaszeuski@gmail.com>
2025-04-15 11:24:46 -0600

    formatting

--- Consilium/Consilium.Maui/Controls/MessagesView.xaml.cs
+++ Consilium/Consilium.Maui/Controls/MessagesView.xaml.cs
@@ -3,8 +3,8 @@ using Consilium.Shared.ViewModels;
 namespace Consilium.Maui.Controls;
 
 public partial class MessagesView : ContentView {
-	public MessagesView() {
-		InitializeComponent();
-		BindingContext = ((App)Application.Current).Services.GetService<MessagesViewModel>();
-	}
+    public MessagesView() {
+        InitializeComponent();
+        BindingContext = ((App)Application.Current).Services.GetService<MessagesViewModel>();
+    }
 }
--- Consilium/Consilium.Maui/Views/ChatView.xaml.cs
+++ Consilium/Consilium.Maui/Views/ChatView.xaml.cs
@@ -2,12 +2,10 @@ using Consilium.Shared.ViewModels;
 
 namespace Consilium.Maui.Views;
 
-public partial class ChatView : ContentPage
-{
-	public ChatView(ChatViewModel vm)
-	{
-		InitializeComponent();
-		BindingContext = vm;
+public partial class ChatView : ContentPage {
+    public ChatView(ChatViewModel vm) {
+        InitializeComponent();
+        BindingContext = vm;
         Appearing += async (s, e) =>
         {
             await vm.InitConversations();
