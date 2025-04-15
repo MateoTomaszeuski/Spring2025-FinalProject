1c7bd87d0a1e7252c67a6e3b01fa8fc6536ef424
mateo <mtomaszeuski@gmail.com>
2025-04-15 12:21:32 -0600

    view

--- Consilium/Consilium.Maui/Controls/MessagesView.xaml.cs
+++ Consilium/Consilium.Maui/Controls/MessagesView.xaml.cs
@@ -7,4 +7,25 @@ public partial class MessagesView : ContentView {
         InitializeComponent();
         BindingContext = ((App)Application.Current).Services.GetService<MessagesViewModel>();
     }
+
+    public static readonly BindableProperty ConversationWithProperty = BindableProperty.Create(
+        propertyName: nameof(ConversationWith),
+        returnType: typeof(string),
+        declaringType: typeof(MessagesView),
+        defaultValue: string.Empty,
+        propertyChanged: OnConversationWithChanged);
+
+    private static void OnConversationWithChanged(BindableObject bindable, object oldValue, object newValue)
+    {
+        var control = (MessagesView)bindable;
+        // Update the view model's property if the BindingContext is of type MessagesViewModel.
+        if (control.BindingContext is MessagesViewModel viewModel)
+        {
+            viewModel.ConversationWith = newValue as string;
+        }
+    }
+    public string ConversationWith {
+        get => (string)GetValue(ConversationWithProperty);
+        set => SetValue(ConversationWithProperty, value);
+    }
 }
