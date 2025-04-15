163ecddbabd3c49e1cbd71ff58ed47fc43719ac2
Cody-Howell <codyhowell44@gmail.com>
2025-04-15 12:51:39 -0600

    added integration test!

--- Consilium/Consilium.IntegrationTests/Tests.cs
+++ Consilium/Consilium.IntegrationTests/Tests.cs
@@ -1,16 +1,39 @@
 ﻿
+using System.Reflection.Metadata;
 using System.Threading.Tasks;
 
 namespace Consilium.IntegrationTests;
 
 public class Tests {
+    private readonly HttpClient client;
+    public Tests() {
+        client = new HttpClient();
+        client.BaseAddress = new Uri("http://localhost:8080");
+        //client.BaseAddress = new Uri("http://consilium-api:8080");
+
+    }
+
     [Test]
     public async Task HelloWorldCall() {
-        var client = new HttpClient();
-        client.BaseAddress = new Uri("http://consilium-api:8080");
-
         string response = await client.GetStringAsync("/health");
 
         await Assert.That(response).IsEqualTo("\"healthy\"");
+    }
+
+    [Test]
+    public async Task FullAuthFlow() {
+        string email = "bob@example.com";
+        string key = "APIKEY0987654321";
+        string token = "TOKENVALID09876543210987654321";
+        var response = await client.GetAsync($"/account/validate?email={email}&token={token}");
+
+        await Assert.That(response.StatusCode.ToString()).IsEqualTo("OK");
+
+        client.DefaultRequestHeaders.Add("Email-Auth_Email", email);
+        client.DefaultRequestHeaders.Add("Email-Auth_ApiKey", key);
+
+        var response2 = await client.GetAsync("/account/valid");
+
+        await Assert.That(response2.IsSuccessStatusCode).IsEqualTo(true);
     }
 }
