d64d24472b972688d7a8f7d8d5b0709afc7a6d83
mateo <mtomaszeuski@gmail.com>
2025-04-15 12:44:36 -0600

    chau method

--- Consilium/Consilium.Maui/Controls/MessagesView.xaml.cs
+++ Consilium/Consilium.Maui/Controls/MessagesView.xaml.cs
@@ -12,16 +12,13 @@ public partial class MessagesView : ContentView {
         propertyName: nameof(ConversationWith),
         returnType: typeof(string),
         declaringType: typeof(MessagesView),
-        propertyChanged: OnConversationWithChanged);
-
-    private static void OnConversationWithChanged(BindableObject bindable, object oldValue, object newValue) {
-        var control = (MessagesView)bindable;
-        // Update the view model's property if the BindingContext is of type MessagesViewModel.
-        if (control.BindingContext is MessagesViewModel viewModel) {
-            // viewModel.ConversationWith = newValue as string;
-            control.ConversationLabel.Text = $"Chat with {viewModel.ConversationWith}";
+        propertyChanged: (bindable, oldValue, newValue) =>
+        {
+            var control = (MessagesView)bindable;
+            control.ConversationLabel.Text = $"Chat with {(string)newValue}";
         }
-    }
+    );
+
     public string ConversationWith {
         get => (string)GetValue(ConversationWithProperty);
         set => SetValue(ConversationWithProperty, value);
