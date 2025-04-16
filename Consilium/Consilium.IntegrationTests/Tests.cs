b89bf472cfb6c6870ba02b83c24917894a016bfb
Cody-Howell <codyhowell44@gmail.com>
2025-04-16 14:17:24 -0600

    sending the integration test!

--- Consilium/Consilium.IntegrationTests/Tests.cs
+++ Consilium/Consilium.IntegrationTests/Tests.cs
@@ -1,6 +1,9 @@
 ﻿
+using Consilium.Shared.Models;
+using System.Net.Http.Json;
 using System.Reflection.Metadata;
 using System.Threading.Tasks;
+using TUnit.Assertions;
 
 namespace Consilium.IntegrationTests;
 
@@ -10,6 +13,12 @@ public class Tests : IDisposable {
         client = new HttpClient();
         //client.BaseAddress = new Uri("http://localhost:8080");
         client.BaseAddress = new Uri("http://consilium-api:8080");
+
+        string email = "bob@example.com";
+        string key = "APIKEY0987654321";
+
+        client.DefaultRequestHeaders.Add("Email-Auth_Email", email);
+        client.DefaultRequestHeaders.Add("Email-Auth_ApiKey", key);
     }
 
     [Test]
@@ -20,20 +29,35 @@ public class Tests : IDisposable {
     }
 
     [Test]
+    [NotInParallel(Order = 1)]
     public async Task FullAuthFlow() {
         string email = "bob@example.com";
-        string key = "APIKEY0987654321";
         string token = "TOKENVALID09876543210987654321";
         var response = await client.GetAsync($"/account/validate?email={email}&token={token}");
 
         await Assert.That(response.StatusCode.ToString()).IsEqualTo("OK");
 
-        client.DefaultRequestHeaders.Add("Email-Auth_Email", email);
-        client.DefaultRequestHeaders.Add("Email-Auth_ApiKey", key);
-
         var response2 = await client.GetAsync("/account/valid");
 
         await Assert.That(response2.IsSuccessStatusCode).IsEqualTo(true);
+    }
+
+    [Test]
+    [NotInParallel(Order = 2)]
+    public async Task AddCourseFlow() {
+        Course c = new Course() { CourseName = "Test" };
+        var response = await client.PostAsJsonAsync("/assignment/course", c);
+
+        await Assert.That(response.IsSuccessStatusCode).IsEqualTo(true);
+
+        var response2 = await client.GetAsync("/assignment/courses");
+        List<Course>? courses = await response2.Content.ReadFromJsonAsync<List<Course>>();
+
+        if (courses is null) { throw new Exception("Courses is null"); }
+
+        await Assert.That(courses.Count).IsEqualTo(1);
+        await Assert.That(courses[0].Id).IsEqualTo(4);
+        await Assert.That(courses[0].CourseName).IsEqualTo("Test");
     }
 
     public void Dispose() {
