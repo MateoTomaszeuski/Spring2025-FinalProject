4e6cbe7da43889b00cd71b2f4de348a39cd1df94
mtoma <mtoma@DESKTOP-P9KVDUO>
2025-04-17 16:59:20 -0600

    byw comments

--- Consilium/Consilium.Maui/Controls/MessagesView.xaml.cs
+++ Consilium/Consilium.Maui/Controls/MessagesView.xaml.cs
@@ -15,28 +15,22 @@ public partial class MessagesView : ContentView {
     }
 
     private void MessagesCollectionView_Loaded(object? sender, EventArgs e) {
-        // first, scroll in case there are already items
         ScrollToLastMessage();
 
-        // then subscribe to the VM's collection changes
         if (BindingContext is MessagesViewModel vm
          && vm.AllMessages is INotifyCollectionChanged notify) {
             notify.CollectionChanged += OnMessagesCollectionChanged;
         }
     }
     private void OnMessagesCollectionChanged(object sender, NotifyCollectionChangedEventArgs e) {
-        // only scroll on inserts
         if (e.Action == NotifyCollectionChangedAction.Add)
             ScrollToLastMessage();
     }
 
     private void ScrollToLastMessage() {
-        // grab the ItemsSource as a list
         if (MessagesCollectionView.ItemsSource is System.Collections.IList list
          && list.Count > 0) {
-            // get the last item
             var lastItem = list[list.Count - 1];
-            // scroll to it
             MessagesCollectionView.ScrollTo(
                 lastItem,
                 position: ScrollToPosition.End,
