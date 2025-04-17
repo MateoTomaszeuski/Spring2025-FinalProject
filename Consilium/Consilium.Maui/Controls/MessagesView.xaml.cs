f818dc302859c84ccc40670c520661a85320ddb6
Audrey Inglish <audreyinglish@gmail.com>
2025-04-16 22:12:55 -0600

    send messages on enter

--- Consilium/Consilium.Maui/Controls/MessagesView.xaml.cs
+++ Consilium/Consilium.Maui/Controls/MessagesView.xaml.cs
@@ -3,9 +3,17 @@ using Consilium.Shared.ViewModels;
 namespace Consilium.Maui.Controls;
 
 public partial class MessagesView : ContentView {
+    private MessagesViewModel vm;
+
     public MessagesView() {
         InitializeComponent();
-        var vm = ((App)Application.Current).Services.GetService<MessagesViewModel>();
+        vm = ((App)Application.Current).Services.GetService<MessagesViewModel>();
         BindingContext = vm;
+    }
+
+    private void MessageContent_Completed(object sender, EventArgs e) {
+        if (vm.SendMessageCommand.CanExecute(null) == true) {
+            vm.SendMessageCommand.Execute(null);
+        }
     }
 }
