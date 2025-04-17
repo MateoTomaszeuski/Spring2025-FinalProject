324af39e10c4103b47dd1f1d52e099726c7e96ba
Audrey Inglish <audreyinglish@gmail.com>
2025-04-16 23:19:46 -0600

    still can't get messages to scroll to the bottom

--- Consilium/Consilium.Maui/Controls/MessagesView.xaml.cs
+++ Consilium/Consilium.Maui/Controls/MessagesView.xaml.cs
@@ -9,11 +9,38 @@ public partial class MessagesView : ContentView {
         InitializeComponent();
         vm = ((App)Application.Current).Services.GetService<MessagesViewModel>();
         BindingContext = vm;
+        ScrollToBottom();
+
+        BindingContextChanged += (_, _) => {
+            if (vm != null) {
+                vm.MessagesUpdated -= ScrollToBottom;
+                vm.MessagesUpdated += ScrollToBottom;
+            }
+        };
     }
 
     private void MessageContent_Completed(object sender, EventArgs e) {
         if (vm.SendMessageCommand.CanExecute(null) == true) {
             vm.SendMessageCommand.Execute(null);
         }
+    }
+
+    // this is scrolling to the top and I don't know why...
+    private void ScrollToBottom() {
+        if (vm?.AllMessages?.Count > 0) {
+            MainThread.BeginInvokeOnMainThread(async () =>
+            {
+                await Task.Delay(100);
+
+                var index = vm.AllMessages.Count - 1;
+                Console.WriteLine($"Trying to scroll to index: {index}");
+
+                MessagesCollectionView.ScrollTo(index, position: ScrollToPosition.End, animate: false);
+            });
+        }
+    }
+
+    private void MessagesCollectionView_Loaded(object sender, EventArgs e) {
+        ScrollToBottom();
     }
 }
