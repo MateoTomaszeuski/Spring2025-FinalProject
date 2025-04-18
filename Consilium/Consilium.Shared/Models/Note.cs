e5ca0882aa2bf17ba04039ddafd3216c0fb05e1c
Tallen258 <112665786+Tallen258@users.noreply.github.com>
2025-04-17 19:55:01 -0600

    started on notes

---
+++ Consilium/Consilium.Shared/Models/Note.cs
@@ -0,0 +1,8 @@
+﻿
+
+namespace Consilium.Shared.Models;
+
+public class Note {
+    public string Title { get; set; }
+    public string Content { get; set; }
+}
--- Consilium/Consilium.Shared/ViewModels/controls/NotesViewModel.cs
+++ Consilium/Consilium.Shared/ViewModels/controls/NotesViewModel.cs
@@ -1,6 +1,35 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Consilium.Shared.Models;
+using System.Collections.ObjectModel;
+using System.Text.Json;
+
 
 namespace Consilium.Shared.ViewModels.Controls;
 
 public partial class NotesViewModel : ObservableObject {
+    private string NotesKey = "saved_notes";
+
+    [ObservableProperty]
+    private string title;
+
+    [ObservableProperty]
+    private string content;
+
+    public ObservableCollection<Note> Notes { get; } = new();
+
+    [RelayCommand]
+    private void AddNote() {
+        if (!string.IsNullOrWhiteSpace(Title) || !string.IsNullOrWhiteSpace(Content)) {
+            Notes.Add(new Note { Title = Title, Content = Content });
+            Title = string.Empty;
+            Content = string.Empty;
+        }
+    }
+    [RelayCommand]
+    private void DeleteNote(Note note) {
+        if (Notes.Contains(note)) {
+            Notes.Remove(note);
+        }
+    }
 }
