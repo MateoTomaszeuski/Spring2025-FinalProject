f997bf1353ed380d77bebec014a82b243db3cda3
audrey.inglish <audrey.inglish@students.snow.edu>
2025-04-18 15:29:08 -0600

    saving works

--- Consilium/Consilium.Maui/Controls/NotesView.xaml.cs
+++ Consilium/Consilium.Maui/Controls/NotesView.xaml.cs
@@ -1,10 +1,20 @@
 using Consilium.Shared.ViewModels.Controls;
+using System.ComponentModel;
 
 namespace Consilium.Maui.Controls;
 
 public partial class NotesView : ContentView {
+    NotesViewModel _viewModel;
     public NotesView() {
         InitializeComponent();
-        BindingContext = ((App)Application.Current).Services.GetService<NotesViewModel>();
+        _viewModel = ((App)Application.Current).Services.GetService<NotesViewModel>();
+        BindingContext = _viewModel;
+        _viewModel.PropertyChanged += PropertyChanged_Handler;
+    }
+
+    private void PropertyChanged_Handler(object? sender, PropertyChangedEventArgs e) {
+        if(e.PropertyName == nameof(_viewModel.Content)) {
+            _viewModel.Dispose();
+        }
     }
 }
