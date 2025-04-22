348f4a52cc33516ce558a85d530eae74936c7f8f
MateoTomaszeuski <mtomaszeuski@gmail.com>
2025-04-22 15:33:02 -0600

    use validation

--- Consilium/Consilium.API/Program.cs
+++ Consilium/Consilium.API/Program.cs
@@ -156,7 +156,7 @@ if (app.Environment.IsDevelopment() || featureFlag) {
 
 app.UseDefaultFiles();
 app.UseStaticFiles();
-// app.UseMiddleware<IdentityMiddleware>();
+app.UseMiddleware<IdentityMiddleware>();
 app.UseRouting();
 
 //change
