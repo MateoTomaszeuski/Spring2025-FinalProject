6c56ffc9501e1207ecc12a2f74e219623239de04
Cody-Howell <codyhowell44@gmail.com>
2025-04-22 21:02:09 -0600

    Updated collapsing header component

--- stt-static-page/src/components/CollapsingHeader.tsx
+++ stt-static-page/src/components/CollapsingHeader.tsx
@@ -1,6 +1,6 @@
-import React, { JSX } from 'react';
+import React, { JSX } from "react";
 
-export type CollapsingHeaderProps = {
+type CollapsingHeaderProps = {
   defaultOpen: boolean,
   headerText: string,
   idRef: string,
@@ -9,47 +9,46 @@ export type CollapsingHeaderProps = {
 }
 
 /**
- * Anything you enclose will be collapsed when you click on the header.
- * You can style like a class header, but I added the arrow that points right or down respectively.
- * Sample below:
- *
+ * Anything you enclose will be collapsed when you click on the header. 
+ * You can style like a class header, but I added the arrow that points right or down respectively. 
+ * Sample below: 
+ * 
  * @example
  * <CollapsingHeader defaultOpen={true} headerText="lorem" idRef="navigateHere" headerType={'h1'}>
  *   <p>Inner paragraph text</p>
  * </CollapsingHeader>
- *
+ * 
  * @param {boolean} defaultOpen - Is open by default when first loaded (for true)
  * @param {string} headerText - Text to be displayed in the header
  * @param {string} idRef - HTML ID to link to
- * @param {keyof JSX.IntrinsicElements} headerType (opt.) - Defaults to 'h2', but all header types can be passed in.
+ * @param {keyof JSX.IntrinsicElements} headerType (opt.) - Defaults to 'h2', but all header types can be passed in. 
  */
-
-export class CollapsingHeader extends React.Component<CollapsingHeaderProps, { isOpen: boolean; isHovered: boolean; isCopied: boolean; }> {
+export class CollapsingHeader extends React.Component<CollapsingHeaderProps, { isOpen: boolean, isHovered: boolean, isCopied: boolean }> {
   constructor(props: CollapsingHeaderProps) {
     super(props);
     this.state = {
       isOpen: this.props.defaultOpen,
       isHovered: false,
       isCopied: false
-    };
-    this.headerRef = React.createRef();
+    }
   }
-  headerRef: React.RefObject<HTMLElement>;
 
   componentDidMount() {
-    // Only scroll if this header is the one targeted by the hash
-    const currentHash = window.location.hash.replace('#', '');
-    if (currentHash === this.props.idRef && this.headerRef.current) {
-      this.headerRef.current.scrollIntoView({ behavior: 'smooth' });
+    if (window.location.hash) {
+      const elementId = window.location.hash.slice(1); // remove the '#' symbol
+      const el = document.getElementById(elementId);
+      if (el) {
+        el.scrollIntoView({ behavior: "smooth", block: "start" });
+      }
     }
   }
 
   toggleOpen = () => {
     this.setState({ isOpen: !this.state.isOpen });
-  };
+  }
 
-  setHoverOn = () => { this.setState({ isHovered: true }); };
-  setHoverOff = () => { this.setState({ isHovered: false }); };
+  setHoverOn = () => { this.setState({ isHovered: true }) };
+  setHoverOff = () => { this.setState({ isHovered: false }) };
   copyLink = () => {
     const fullLink = `${window.location.origin}${window.location.pathname}#${this.props.idRef}`;
     navigator.clipboard.writeText(fullLink);
@@ -57,17 +56,15 @@ export class CollapsingHeader extends React.Component<CollapsingHeaderProps, { i
     setTimeout(() => {
       this.setState({ isCopied: false });
     }, 2000);
-  };
+  }
 
   render() {
     const HeaderTag = this.props.headerType || 'h2'; // default if none provided
 
-    // Don't mind the error here, everything works as intended anyways. 
     return (
       <>
         <HeaderTag onClick={this.toggleOpen}
           id={this.props.idRef}
-          ref={this.headerRef}
           onMouseEnter={this.setHoverOn}
           onMouseLeave={this.setHoverOff}>
           {this.state.isOpen ? <span>&#11206;</span> : <span>&#11208;</span>} {this.props.headerText} {this.state.isHovered && (
@@ -76,6 +73,6 @@ export class CollapsingHeader extends React.Component<CollapsingHeaderProps, { i
         </HeaderTag>
         {this.state.isOpen && (this.props.children)}
       </>
-    );
+    )
   }
 }
