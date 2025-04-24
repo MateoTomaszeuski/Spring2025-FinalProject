72676d44be6f6abaca15735eec690d9325e0594f
MateoTomaszeuski <mtomaszeuski@gmail.com>
2025-04-24 14:52:59 -0600

    new metric to calculate persentaje

--- Consilium/Consilium.API/Program.cs
+++ Consilium/Consilium.API/Program.cs
@@ -30,6 +30,9 @@ DateTime started = DateTime.UtcNow;
 builder.Logging.AddConsole();
 
 double previousAggregated = LoadAggregatedUptimeFromCsv(UptimeCsvPath);
+var firstStart = LoadFirstDeploymentStartTime(UptimeCsvPath)
+                   ?? DateTime.UtcNow;
+
 var meter = new Meter(serviceName, "1.0.0");
 var errorCountCounter = meter.CreateCounter<long>("error_count", description: "Total number of errors");
 var errorMessageCounter = meter.CreateCounter<long>("error_messages", description: "Number of errors by message", unit: "1");
@@ -50,7 +53,24 @@ meter.CreateObservableGauge(
     },
     description: "Aggregated application uptime in seconds"
 );
+meter.CreateObservableGauge(
+    "application_uptime_percentage",
+    () =>
+    {
+        var currentSecs = (DateTime.UtcNow - started).TotalSeconds;
 
+        var totalUp = previousAggregated + currentSecs;
+
+        var windowSecs = (DateTime.UtcNow - firstStart).TotalSeconds;
+
+        var pct = windowSecs > 0
+                  ? (totalUp / windowSecs) * 100
+                  : 0;
+
+        return new[] { new Measurement<double>(pct) };
+    },
+    description: "Percent of time healthy since first deployment"
+);
 var myConcurrentUserTracker = new MyConcurrentUserTracker();
 meter.CreateObservableGauge(
     "concurrent_users",
@@ -247,6 +267,33 @@ void AppendUptimeRow(string path, string podName, DateTime shutdownUtc, double u
     // use ISO 8601 for the timestamp
     var ts = shutdownUtc.ToString("o", CultureInfo.InvariantCulture);
     w.WriteLine($"{podName},{ts},{uptimeSeconds.ToString(CultureInfo.InvariantCulture)}");
+}
+DateTime? LoadFirstDeploymentStartTime(string csvPath) {
+    if (!File.Exists(csvPath))
+        return null;
+
+    // skip header, grab the first data row
+    var firstLine = File.ReadAllLines(csvPath).Skip(1).FirstOrDefault();
+    if (string.IsNullOrEmpty(firstLine))
+        return null;
+
+    var cols = firstLine.Split(',');
+    if (cols.Length < 3)
+        return null;
+
+    if (!DateTime.TryParse(cols[1],
+                           null,
+                           DateTimeStyles.RoundtripKind,
+                           out var shutdown))
+        return null;
+
+    if (!double.TryParse(cols[2],
+                         NumberStyles.Float,
+                         CultureInfo.InvariantCulture,
+                         out var uptimeSecs))
+        return shutdown;
+
+    return shutdown - TimeSpan.FromSeconds(uptimeSecs);
 }
 
 public class MyConcurrentUserTracker {
