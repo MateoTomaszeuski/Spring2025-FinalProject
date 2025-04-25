a8afccd8161a890a20ca9e01730a754665d5a4b8
mtoma <mtoma@DESKTOP-P9KVDUO>
2025-04-25 12:02:05 -0600

    udpate

--- Consilium/Consilium.Maui/MauiProgram.cs
+++ Consilium/Consilium.Maui/MauiProgram.cs
@@ -38,8 +38,8 @@ public static class MauiProgram {
 #endif
         builder.Services.AddHttpClient("ApiClient", client =>
         {
-            client.BaseAddress = new Uri("https://consilium-api-cpgdcqaxepbyc2gj.westus3-01.azurewebsites.net/");
-            // client.BaseAddress = new Uri("https://main.consilium.duckdns.org/"); // If you want to hit kubernetees, make sure to be in the vpn
+             client.BaseAddress = new Uri("https://consilium-api-cpgdcqaxepbyc2gj.westus3-01.azurewebsites.net/");
+            //client.BaseAddress = new Uri("https://main.consilium.duckdns.org/"); // If you want to hit kubernetees, make sure to be in the vpn
 
 #if DEBUG
             //client.BaseAddress = new Uri("http://localhost:5202");
@@ -75,6 +75,7 @@ public static class MauiProgram {
 
         return builder;
     }
+
 
     public static MauiAppBuilder RegisterViewModels(this MauiAppBuilder builder) {
         builder.Services.AddSingleton<AssignmentsViewModel>();
